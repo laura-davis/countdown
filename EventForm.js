1bb4eb3094313e209075e84d2d6595215f0aa12d
laura-davis <laura.davis@sky.uk>
2020-05-17 22:33:10 +0100

    Completed section 6 of react native getting started PL course

--- EventForm.js
+++ EventForm.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import {Text, View, TouchableHighlight, TextInput, StyleSheet} from "react-native";
+import DateTimePicker from "react-native-modal-datetime-picker";
+import {formatDateTime} from "./api";
 
 export default class EventForm extends React.Component {
+    state = {
+        title: null,
+        date: ''
+    };
+
     handleAddPress = () => {
+        console.log(this.state);
         this.props.navigation.navigate('list');
     };
+
+    handleChangeTitle = (value) => {
+        this.setState({title: value})
+    };
+
+    handleDatePress = () => {
+        this.setState({showDatePicker: true})
+    };
+
+    handleDatePicked = (date) => {
+        this.setState({
+            date
+        });
+        this.handleDatePickerHide();
+    };
+
+    handleDatePickerHide = () => {
+        this.setState({showDatePicker: false})
+    };
+
 
     render() {
         return (
@@ -12,10 +40,27 @@ export default class EventForm extends React.Component {
                 flex: 1,
             }}>
                 <View style={styles.fieldContainer}>
-                    <TextInput style={styles.text}/>
+                    <TextInput style={styles.text}
+                               placeholder="Event title"
+                               spellCheck={false}
+                               value={this.state.title}
+                               onChangeText={this.handleChangeTitle}
+                    />
+                    <TextInput style={[styles.text, styles.borderTop]}
+                    placeholder="Event date"
+                    spellCheck={false}
+                    value={formatDateTime(this.state.date.toString())}
+                    editable={!this.state.showDatePicker}
+                    onFocus={this.handleDatePress}/>
+                    <DateTimePicker
+                    isVisible={this.state.showDatePicker}
+                    mode="datetime"
+                    onConfirm={this.handleDatePicked}
+                    onCancel={this.handleDatePickerHide}/>
                 </View>
-                <TouchableHighlight onPress={this.handleAddPress}>
-                    <Text>Add</Text>
+                <TouchableHighlight onPress={this.handleAddPress}
+                style={styles.button}>
+                    <Text style={styles.buttonText}>Add</Text>
                 </TouchableHighlight>
             </View>
         );
@@ -23,15 +68,33 @@ export default class EventForm extends React.Component {
 }
 
 const styles = StyleSheet.create({
-fieldContainer: {
-    marginTop: 20,
-    marginBottom: 20,
-    backgroundColor: '#fff'
-},
-    text:  {
-    height: 40,
+    fieldContainer: {
+        marginTop: 20,
+        marginBottom: 20,
+        backgroundColor: '#fff'
+    },
+    text: {
+        height: 40,
         margin: 0,
         marginRight: 7,
         paddingLeft: 10
+    },
+    button: {
+        height: 50,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        alignSelf: 'stretch',
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+    },
+    borderTop: {
+        borderColor: '#edeeef',
+        borderTopWidth: 0.5,
     }
 });
